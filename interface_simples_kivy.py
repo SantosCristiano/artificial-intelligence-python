c16ed0ebb7e0bb4a74de52eb6f9f66f079436cc8
Christiano Sanctorum <santosdesouzacris@gmail.com>
2024-05-28 19:05:21 -0300

    Update .gitignore

--- interface_simples_kivy.py
+++ interface_simples_kivy.py
@@ -1,19 +1,21 @@
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
-from kivy.uix.label import Label
 from kivy.uix.button import Button
 from kivy.uix.textinput import TextInput
 import webbrowser
-def msg():
-        print("Sigue aprendiendo en : https://inteligencia-artificial.dev/formacion")
+
 class Aplicacion(App):
     def build(self):
         layout = BoxLayout(orientation="vertical")
         boton1 = Button(text="Abrir web")
         web = TextInput(text="...")
-        boton1.bind(on_press= lambda a: webbrowser.open_new(web.text))
+        boton1.bind(on_press=lambda a: webbrowser.open_new(web.text))
         layout.add_widget(web)
         layout.add_widget(boton1)
         return layout
-Aplicacion().run()
-msg()
+
+    def on_stop(self):
+        print("Siga aprendendo: https://inteligencia-artificial.dev/formacion")
+
+if __name__ == "__main__":
+    Aplicacion().run()
